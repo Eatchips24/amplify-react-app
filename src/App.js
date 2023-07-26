9112b8815dfe86eb77b9e897b30aafff93548288
Eatchips24 <130024103+Eatchips24@users.noreply.github.com>
2023-07-26 14:55:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Where is Messi?
+          Edit <code>src/App.js</code> Who goes there?
         </p>
         <a
           className="App-link"
